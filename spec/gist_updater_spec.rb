66964528a72051057a475ca4fc04459e2abd16b9
Takashi Masuda <masutaka.net@gmail.com>
2016-09-24 23:46:33 +0900

    Make `$ rake spec` to green

--- spec/gist_updater_spec.rb
+++ spec/gist_updater_spec.rb
@@ -6,8 +6,4 @@ describe GistUpdater do
   it 'has a version number' do
     expect(GistUpdater::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
