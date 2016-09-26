2e9360403948a5eb4f30e100a9df898465155702
Takashi Masuda <masutaka.net@gmail.com>
2016-09-26 23:37:39 +0900

    Fix bug

--- lib/gist_updater/updater.rb
+++ lib/gist_updater/updater.rb
@@ -4,7 +4,7 @@ module GistUpdater
   class Updater
     def initialize(options, config_class = Config)
       @user             = options[:user] || ENV['GISTUPDATER_USER']
-      @access_token     = options[:access_token] || ENV['GISTUPDATER_ACCESS_TOKEN']
+      @access_token     = options[:token] || ENV['GISTUPDATER_ACCESS_TOKEN']
       GistUpdater.debug = options[:debug]
       @config           = config_class.new(options[:yaml])
     end
